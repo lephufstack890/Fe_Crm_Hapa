c607d845c6d439f26d3f3a5267b47fc9680b438c
lephu <tui.hrv@gmail.com>
2025-01-16 17:36:39 +0700

    refactor code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,11 @@
 import MainLayout from '@components/layouts/main-layout';
 import MainUserLayout from '@components/layouts/user/main-user-layout';
-import HomePage from '@components/pages/home-page/home-page';
-import LoginPage from '@components/pages/login-page/login-page';
-import AccountPage from '@components/pages/user/account-page/account-page';
-import FilterReplacementSchedulePage from '@components/pages/user/filter-replacement-schedule-page/filter-replacement-schedule-page';
-import OrderDetailPage from '@components/pages/user/order-detail-page/order-detail-page';
-import OrderPage from '@components/pages/user/orders-page/order-page';
+import HomePage from '@components/pages/customer/home-page/home-page';
+import LoginPage from '@components/pages/customer/login-page/login-page';
+import AccountPage from '@components/pages/customer/account-page/account-page';
+import FilterReplacementSchedulePage from '@components/pages/customer/filter-replacement-schedule-page/filter-replacement-schedule-page';
+import OrderDetailPage from '@components/pages/customer/order-detail-page/order-detail-page';
+import OrderPage from '@components/pages/customer/orders-page/order-page';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
